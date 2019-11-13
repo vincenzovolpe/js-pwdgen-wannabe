dbfcde652f9cd29572f4cec8197c8db0b8c77d7f
vincenzovolpe <vinci792010@gmail.com>
2019-11-13 15:59:26 +0100

    Piccoli abbellimenti

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var nome = prompt('Come ti chiami?')
 
 var cognome = prompt("Qual'è  il tuo cognome?")
 
-var colore = prompt("Qual'è  il tuo colore preferito?")
+var colore = prompt("Qual'è  il tuo colore preferito? (digitalo in inglese)")
 
 var numero = prompt("Digita un numero intero: ")
 
@@ -15,3 +15,6 @@ document.getElementById('messaggio').setAttribute("class", "visibile");
 
 //Aggiunta password al messaggio a schermo
 document.getElementById('password').innerHTML = password;
+
+//Aggiunta colore della password in base al colore scelto solo se viene scritto in inglese
+document.getElementById('password').style.color = colore;
